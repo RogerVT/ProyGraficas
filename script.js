2d47b96bbb100f727c03158369979d4df0bdf76a
Roger Vazquez <revazqueztuz@gmail.com>
2021-09-29 13:34:49 -0500

    Base proyect added

---
+++ script.js
@@ -0,0 +1,14 @@
+function main() {
+    const canvas = document.querySelector('#glCanvas');
+    const gl = canvas.getContext('webgl');
+
+    if (gl == null) {
+        alert('WebGL not supported');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+} 
+
+window.onload = main; 
